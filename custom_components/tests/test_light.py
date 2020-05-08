66ad79e2740ece05421bd983b4817634cd9e527b
BazaJayGee66 <BazaJayGee66>
2020-05-08 19:27:40 +1000

    Add tests

---
+++ custom_components/tests/test_light.py
@@ -0,0 +1,154 @@
+import pytest
+import socket
+import sys
+
+import cololight
+
+from unittest.mock import patch, call
+
+# Link to homeassistant module
+sys.path.insert(1, "/homeassistant/core")
+from tests.conftest import hass, hass_storage
+from homeassistant.const import ATTR_ENTITY_ID, STATE_OFF, STATE_ON
+from homeassistant.components.light import (
+    ATTR_BRIGHTNESS,
+    ATTR_EFFECT,
+    ATTR_HS_COLOR,
+    DOMAIN as LIGHT_DOMAIN,
+    SERVICE_TURN_OFF,
+    SERVICE_TURN_ON,
+)
+from homeassistant.setup import async_setup_component
+
+CONF_NAME = "cololight_test"
+ENTITY_LIGHT = f"light.{CONF_NAME}"
+
+
+@pytest.fixture(autouse=True)
+@patch("socket.socket")
+def setup_comp(mock_socket, hass):
+    """Set up cololight component."""
+    hass.loop.run_until_complete(
+        async_setup_component(
+            hass,
+            LIGHT_DOMAIN,
+            {
+                LIGHT_DOMAIN: {
+                    "platform": "cololight",
+                    "name": CONF_NAME,
+                    "host": "1.1.1.1",
+                }
+            },
+        )
+    )
+
+
+@patch("homeassistant.components.cololight.light.coloLight.send_message")
+async def test_turn_on(mock_sending, hass):
+    """Test the light turns of successfully."""
+
+    await hass.services.async_call(
+        LIGHT_DOMAIN, SERVICE_TURN_ON, {ATTR_ENTITY_ID: ENTITY_LIGHT}, blocking=True,
+    )
+
+    state = hass.states.get(ENTITY_LIGHT)
+
+    mock_sending.assert_called_with(
+        b"SZ00\x00\x00\x00\x00\x00 \x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x04\x01\x03\x01\xcfd"
+    )
+    assert state.state == STATE_ON
+    assert state.attributes.get(ATTR_BRIGHTNESS) == 255
+
+
+@patch("homeassistant.components.cololight.light.coloLight.send_message")
+async def test_turn_on_with_brightness(mock_sending, hass):
+    """Test the light turns on to the specified brightness."""
+
+    await hass.services.async_call(
+        LIGHT_DOMAIN,
+        SERVICE_TURN_ON,
+        {ATTR_ENTITY_ID: ENTITY_LIGHT, ATTR_BRIGHTNESS: 60},
+        blocking=True,
+    )
+
+    state = hass.states.get(ENTITY_LIGHT)
+
+    mock_sending.assert_called_with(
+        b"SZ00\x00\x00\x00\x00\x00 \x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x04\x01\x03\x01\xcf\x17"
+    )
+    assert state.state == STATE_ON
+    assert state.attributes.get(ATTR_BRIGHTNESS) == 60
+
+
+@patch("homeassistant.components.cololight.light.coloLight.send_message")
+async def test_turn_on_with_effect(mock_sending, hass):
+    """Test the light turns on with effect."""
+
+    await hass.services.async_call(
+        LIGHT_DOMAIN,
+        SERVICE_TURN_ON,
+        {ATTR_ENTITY_ID: ENTITY_LIGHT, ATTR_EFFECT: "Sunrise", ATTR_BRIGHTNESS: 60},
+        blocking=True,
+    )
+
+    state = hass.states.get(ENTITY_LIGHT)
+
+    expected_messages = [
+        call(
+            b"SZ00\x00\x00\x00\x00\x00#\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x04\x01\x06\x02\xff\x01\xc1\n\x00"
+        ),
+        call(
+            b"SZ00\x00\x00\x00\x00\x00 \x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x04\x01\x03\x01\xcf\x17"
+        ),
+    ]
+    mock_sending.assert_has_calls(expected_messages)
+    assert mock_sending.call_count == 2
+
+    assert state.state == STATE_ON
+    assert state.attributes.get(ATTR_BRIGHTNESS) == 60
+    assert state.attributes.get(ATTR_EFFECT) == "Sunrise"
+
+
+@patch("homeassistant.components.cololight.light.coloLight.send_message")
+async def test_turn_on_with_colour(mock_sending, hass):
+    """Test the light turns on with colour."""
+
+    await hass.services.async_call(
+        LIGHT_DOMAIN,
+        SERVICE_TURN_ON,
+        {ATTR_ENTITY_ID: ENTITY_LIGHT, ATTR_HS_COLOR: (180, 50), ATTR_BRIGHTNESS: 60},
+        blocking=True,
+    )
+
+    state = hass.states.get(ENTITY_LIGHT)
+
+    expected_messages = [
+        call(
+            b"SZ00\x00\x00\x00\x00\x00#\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x04\x01\x06\x02\xff\x00\x7f\xff\xff"
+        ),
+        call(
+            b"SZ00\x00\x00\x00\x00\x00 \x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x04\x01\x03\x01\xcf\x17"
+        ),
+    ]
+    mock_sending.assert_has_calls(expected_messages)
+    assert mock_sending.call_count == 2
+
+    assert state.state == STATE_ON
+    assert state.attributes.get(ATTR_BRIGHTNESS) == 60
+    assert state.attributes.get(ATTR_HS_COLOR) == (180, 50)
+
+
+@patch("homeassistant.components.cololight.light.coloLight.send_message")
+async def test_turn_off(mock_sending, hass):
+    """Test the light turns off successfully."""
+
+    await hass.services.async_call(
+        LIGHT_DOMAIN, SERVICE_TURN_OFF, {ATTR_ENTITY_ID: ENTITY_LIGHT}, blocking=True,
+    )
+
+    state = hass.states.get(ENTITY_LIGHT)
+
+    mock_sending.assert_called_with(
+        b"SZ00\x00\x00\x00\x00\x00 \x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x04\x01\x03\x01\xce\x1e"
+    )
+    assert state.state == STATE_OFF
