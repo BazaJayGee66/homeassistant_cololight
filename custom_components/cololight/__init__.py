ae9f90cdf7dc2b29bdb9a488ab3a1f5c17a7d996
BazaJayGee66 <BazaJayGee66>
2020-06-08 08:10:56 +1000

    Add removing of entity through UI

--- custom_components/cololight/__init__.py
+++ custom_components/cololight/__init__.py
@@ -18,3 +18,8 @@ async def async_setup_entry(hass: HomeAssistantType, entry: ConfigEntry):
         hass.config_entries.async_forward_entry_setup(entry, LIGHT_DOMAIN)
     )
     return True
+
+
+async def async_unload_entry(hass, entry):
+    """Unload a config entry."""
+    return await hass.config_entries.async_forward_entry_unload(entry, LIGHT_DOMAIN)
