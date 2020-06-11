b4ea78afa11290cea714f5a7db5062a2b30aead1
BazaJayGee66 <BazaJayGee66>
2020-06-11 22:19:03 +1000

    Add creatation of cololight effect to UI

--- custom_components/cololight/__init__.py
+++ custom_components/cololight/__init__.py
@@ -2,6 +2,7 @@
 from homeassistant.components.light import DOMAIN as LIGHT_DOMAIN
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.helpers.typing import HomeAssistantType
+from homeassistant.const import CONF_MODE
 
 DOMAIN = "cololight"
 
@@ -14,6 +15,8 @@ async def async_setup(hass, config):
 async def async_setup_entry(hass: HomeAssistantType, entry: ConfigEntry):
     """Load the saved entities."""
 
+    entry.add_update_listener(update_listener)
+
     hass.async_create_task(
         hass.config_entries.async_forward_entry_setup(entry, LIGHT_DOMAIN)
     )
@@ -23,3 +26,15 @@ async def async_setup_entry(hass: HomeAssistantType, entry: ConfigEntry):
 async def async_unload_entry(hass, entry):
     """Unload a config entry."""
     return await hass.config_entries.async_forward_entry_unload(entry, LIGHT_DOMAIN)
+
+
+async def update_listener(hass, entry):
+    """Handle options update."""
+    for effect_name, effect_options in entry.options.items():
+        hass.data[DOMAIN][entry.entry_id].add_custom_effect(
+            effect_name,
+            effect_options["color_scheme"],
+            effect_options["color"],
+            effect_options["cycle_speed"],
+            effect_options[CONF_MODE],
+        )
