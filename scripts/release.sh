9f6513b356958da282597ed62b994ef1c3d8d372
BazaJayGee66 <jarrad.goodwin66@gmail.com>
2021-02-06 12:13:18 +1100

    Update release zip to exclude sub dir

--- scripts/release.sh
+++ scripts/release.sh
@@ -5,12 +5,13 @@ RELEASE_NOTES=$(buildkite-agent meta-data get release-notes)
 
 echo "+++ :rocket: Releasing version ${RELEASE_VERSION}"
 
+cd custom_components &&\
 zip -r cololight.zip \
-    custom_components/cololight \
+    cololight \
     -x "*/__pycache__/*"
 
 gh release create \
     ${RELEASE_VERSION} \
-    'cololight.zip' \
+    'custom_components/cololight.zip' \
     -t ${RELEASE_VERSION} \
     -n "${RELEASE_NOTES}"
