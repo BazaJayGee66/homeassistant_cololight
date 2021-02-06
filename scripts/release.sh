ae3284aa0adf07fb386f0851b47640a7c4a0766a
BazaJayGee66 <jarrad.goodwin66@gmail.com>
2021-02-06 11:33:10 +1100

    Fix github release

--- scripts/release.sh
+++ scripts/release.sh
@@ -4,14 +4,13 @@ RELEASE_VERSION=$(buildkite-agent meta-data get release-version)
 RELEASE_NOTES=$(buildkite-agent meta-data get release-notes)
 
 echo "+++ :rocket: Releasing version ${RELEASE_VERSION}"
+
 zip -r cololight.zip \
     custom_components/cololight \
     -x "*/__pycache__/*"
-
-gh auth login --with-token <<< "${GITHUB_TOKEN}"
 
 gh release create \
     ${RELEASE_VERSION} \
     'cololight.zip' \
     -t ${RELEASE_VERSION} \
-    -n ${RELEASE_NOTES}
+    -n "${RELEASE_NOTES}"
